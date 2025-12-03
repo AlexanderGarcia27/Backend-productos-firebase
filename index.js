a4c4d6e9c0ecd0c5d44d63641aeee3fbd5074081
Alexander García <agr270404@gmail.com>
2025-12-03 14:59:15 -0600

    4 commit

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const cors = require('cors');
 
 const app = exprees();
 app.use(cors({ origin: true }));
-app.use(exprees.json());    
+app.use(exprees.json());
 
 const COLLECTION = "productos"
 
@@ -12,7 +12,7 @@ app.post("/productos", async (req, res) => {
     try {
         const data = req.body;
 
-        const required = [
+        const requiredFields = [
             "cantidadStock",
             "codigoBarra",
             "fechaCompra",
@@ -21,71 +21,80 @@ app.post("/productos", async (req, res) => {
             "precioCompra",
             "precioVenta",
             "proveedor",
-            "fechaVencimiento"
+            "fechaVencimiento",
         ];
-
-        for (const campo of required) {
-            if (!data[campo] && data[campo] !== 0) {
+        for (const field of requiredFields) {
+            if (
+                data[field] === undefined ||
+                data[field] === null ||
+                data[field] === "" ||
+                (typeof data[field] === "number" && data[field] <= 0)
+            ) {
                 return res.status(400).json({
-                    mensaje: `Falta el campo obligatorio: ${campo}`
+                    mensaje: `Falta o es inválido el campo obligatorio: ${field}`,
                 });
             }
         }
-        if (!Number.isInteger(data.cantidadStock) || data.cantidadStock <= 0) {
+        if (!Number.isInteger(data.cantidadStock)) {
             return res.status(400).json({
-                mensaje: "El stock debe ser un número entero o mayor a 0"
+                mensaje: "El stock debe ser un número entero mayor a 0",
             });
         }
         const existente = await db
             .collection(COLLECTION)
             .where("codigoBarra", "==", data.codigoBarra)
             .get();
+
         if (!existente.empty) {
             return res.status(400).json({
-                mensaje: "El código de barra ya existe en otro producto"
+                mensaje: "El código de barra ya existe en otro producto",
             });
         }
         const nuevo = await db.collection(COLLECTION).add(data);
+
         res.json({
             id: nuevo.id,
-            mensaje: "El producto se guardó"
+            mensaje: "Producto guardado exitosamente",
         });
+
     } catch (error) {
+        console.log(error);
         res.status(500).json({ error: error.message });
     }
 });
 
-app.get("/productos", async (req, res)=>{
-    try{
+
+app.get("/productos", async (req, res) => {
+    try {
         const snapshot = await db.collection(COLLECTION).get();
-        const productos = snapshot.docs.map(doc =>({
+        const productos = snapshot.docs.map(doc => ({
             id: doc.id,
             ...doc.data()
         }));
 
         res.json(productos);
-    } catch(error){
-        res.status(500).json({error: error.message});
+    } catch (error) {
+        res.status(500).json({ error: error.message });
     }
 })
 
-app.get("/productos/codigo/:codigo", async(req, res)=>{
-    try{
+app.get("/productos/codigo/:codigo", async (req, res) => {
+    try {
         const codigo = req.params.codigo;
         const snapshot = await db.collection(COLLECTION).where("codigoBarra", "==", codigo).get();
-        if(snapshot.empty){
-            return res.status(404).json({mensaje: "Producto no encontrado"});
+        if (snapshot.empty) {
+            return res.status(404).json({ mensaje: "Producto no encontrado" });
         }
-        const productos = snapshot.docs.map(doc =>({
+        const productos = snapshot.docs.map(doc => ({
             id: doc.id,
             ...doc.data()
         }));
         res.json(productos[0])
-    }catch (error){
-        res.status(500).json({error: error.message})
+    } catch (error) {
+        res.status(500).json({ error: error.message })
     }
 })
 
 app.listen(3000, () => {
-  console.log("Servidor ejecutándose en http://localhost:3000");
+    console.log("Servidor ejecutándose en http://localhost:3000");
 });
